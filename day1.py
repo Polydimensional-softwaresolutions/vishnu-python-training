e720346f52b6983ce6b4cd75027eb771c057813c
vishnupolydimensional <vishnu.polydimensional@gmail.com>
2025-09-19 12:57:57 +0530

    day1 progress

---
+++ day1.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+hobby = input("Enter your favorite hobby: ")
+
+bio = (f"""Hello! My name is {name}.
+I am {age} years old and I live in {city}.
+In my free time, I enjoy {hobby}.""")
+
+# Print the biography in a clean format
+print(bio)
