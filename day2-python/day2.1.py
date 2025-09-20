143f821089238c6511e444419d27f0fbbec62e1f
vishnupolydimensional <vishnu.polydimensional@gmail.com>
2025-09-20 18:04:39 +0530

    day2 progress

---
+++ day2-python/day2.1.py
@@ -0,0 +1,6 @@
+print("This is a bill and tip calculator")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+pay_per_person = round((bill + (bill * tip / 100)) / people, 2)
+print(f"Each person should pay: ${pay_per_person}")
---
+++ day2-python/day2.py
@@ -0,0 +1,5 @@
+str = "pyThOn iS aMazInG"
+
+transformed = str.lower().capitalize()
+
+print("Transformed string:", transformed)
