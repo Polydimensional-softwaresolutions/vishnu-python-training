b6c74374f0b6fc809bf66e64bd6fdf53e32d26b8
vishnupolydimensional <vishnu.polydimensional@gmail.com>
2025-09-24 12:08:57 +0530

    day5 progress

--- day5-python/day5_0.py
+++ day5-python/day5_0.py
@@ -1,17 +1,24 @@
 fruits = []
-for i in range(5):
-    fruit = input(f"Enter your favorite fruit #{i+1}: ")
-    fruits.append(fruit)
+fruit1 = input("enter your 1st fav fruit:")
+fruit2 = input("enter your 2nd fav fruit:")
+fruit3 = input("enter your 3rd fav fruit:")
+fruit4 = input("enter your 4th fav fruit:")
+fruit5 = input("enter your 5th fav fruit:")
 
-print("\nYour favorite fruits are:", fruits)
+fruits.append(fruit1)
+fruits.append(fruit2)
+fruits.append(fruit3)
+fruits.append(fruit4)
+fruits.append(fruit5)
+
+print("Your favorite fruits are:", fruits)
 
 print("First fruit:", fruits[0])
 print("Last fruit:", fruits[-1])
 print("Middle fruit:", fruits[2])
 
-
 fruits[1] = "Mango"
-print("\nAfter replacing the second fruit:", fruits)
+print("After replacing the second fruit:", fruits)
 
 fruits.append("Watermelon")
 print("After adding Watermelon:", fruits)
